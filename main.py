d1e7d417ddc3d8e7c3ff8ea7c4e63bd9569af840
anne-colombe <anne-colombe.andre@edu.esiee.fr>
2024-09-20 13:39:21 +0000

    voici mon travail sur les palindromes

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
+"Module pour vérifier si une chaîne de caractères est un palindrome."
+import re
+from unidecode import unidecode
+
 #### Fonction secondaire
+def ispalindrome(p:str)->bool:
+    "fonction qui a pour but de vérifier si une chaine de caractère est un palindrome ou pas"
+
+    #on enleve les caractères spéciaux et les majuscules deviennent des minuscules
+    p = unidecode(p)
+    p = re.sub(r'[^a-zA-Z0-9]', '', p)
+    p = p.lower()  # Met tout en minuscules
 
 
-def ispalindrome(p):
+    #on inverse la chaine
+    s_inverse = p[::-1]
 
-    # votre code ici
-    
-    return False
+    #on vérifie que la chaine soit un palindrome
+    return p==s_inverse
 
 #### Fonction principale
-
-
 def main():
+    "cette fonction teste une liste de caractere"
 
-    # vos appels à la fonction secondaire ici
-
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
-        print(s, ispalindrome(s))
+    test_cases = [
+        "radar",
+        "kayak",
+        "level",
+        "rotor",
+        "deified",
+        "Bonjour",
+        "A man a plan a canal Panama",
+        "civique"
+    ]
+    for s in test_cases:
+        result = ispalindrome(s)
+        if result:
+            print(f"Le mot '{s}' est un palindrome.")
+        else:
+            print(f"Le mot '{s}' n'est pas un palindrome.")
 
 
 if __name__ == "__main__":
